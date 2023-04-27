959bd86df48d2b97404561694ed0d90054b9bb51
Andrii Shmerchuk <andrii.shmerchuk@MacBook-Pro-von-Andrii.local>
2023-04-27 15:16:27 +0200

    added functionallity to save all todos in file

--- src/Service/TodoService.php
+++ src/Service/TodoService.php
@@ -3,26 +3,33 @@
 namespace App\Service;
 
 use App\Repository\TodoCookieRepository;
+use App\Repository\TodoFileRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class TodoService
 {
-    public function add_todo(string $sanitaseNameOfSkill){
-        $todoRepository = new TodoCookieRepository();
-        $allTodos = $todoRepository->get_all_todos();
-        foreach ($allTodos as $todo){
-            if($sanitaseNameOfSkill === $todo){
-                return ;
+    private TodoFileRepository $Repository;
+    public function __construct(){
+        $this->Repository = new TodoFileRepository();
+    }
+    public function add_todo(string $sanitaseNameOfSkill)
+    {
+        $allTodos = $this->Repository->get_all_todos();
+        foreach ($allTodos as $todo) {
+            if ($sanitaseNameOfSkill === $todo) {
+                return;
             }
         }
-        $todoRepository->save_todo($sanitaseNameOfSkill);
+        $this->Repository->save_todo($sanitaseNameOfSkill);
     }
-    public function delete_todo($nameOfSkills){
-        $todoRepository = new TodoCookieRepository();
-        $todoRepository->delete_todo($nameOfSkills);
+
+    public function delete_todo($nameOfSkills)
+    {
+        $this->Repository->delete_todo($nameOfSkills);
     }
-    public function get_all_todos(){
-        $todoRepository = new TodoCookieRepository();
-        return $todoRepository->get_all_todos();
+
+    public function get_all_todos()
+    {
+        return $this->Repository->get_all_todos();
     }
 }
